bb0e302b35e0d3a7b15ee5f5a2772e0966f03f31
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-09-11 14:14:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
 	for(let i=0;i<encodedStr.length;i++){
-		if(encodedStr.charAt(i)>=65 && encodedStr.charAt(i)<=90){
+		if(encodedStr.charAt(i)>="A" && encodedStr.charAt(i)<="Z"){
 		decodedArr[i]=lookup[encodedStr.charAt(i)];
 		}
 		else{
@@ -43,7 +43,7 @@ function rot13(encodedStr) {
 		}
 	}
    
-  return decodedAr.join("");
+  return decodedArr.join("");
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
